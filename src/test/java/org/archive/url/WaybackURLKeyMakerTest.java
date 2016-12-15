e3936435fa5dc6d06627742bd0ba6ad6bc8e2e11
Sebastian Nagel <sebastian@commoncrawl.org>
2016-12-15 13:18:41 +0100

    unit tests for SURT keys from non-utf-8 encoded URLs, tests #6

--- src/test/java/org/archive/url/WaybackURLKeyMakerTest.java
+++ src/test/java/org/archive/url/WaybackURLKeyMakerTest.java
@@ -23,6 +23,10 @@ public class WaybackURLKeyMakerTest extends TestCase {
 		assertEquals("org,archive)/goo?a&b", km.makeKey("http://archive.org/goo/?b&a"));
 		assertEquals("org,archive)/goo?a=1&a=2&b", km.makeKey("http://archive.org/goo/?a=2&b&a=1"));
 		assertEquals("org,archive)/", km.makeKey("http://archive.org:/"));
+		assertEquals("ua,1kr)/newslist.html?tag=%e4%ee%f8%ea%ee%eb%fc%ed%ee%e5",
+				km.makeKey("http://1kr.ua/newslist.html?tag=%E4%EE%F8%EA%EE%EB%FC%ED%EE%E5"));
+		assertEquals("com,aluroba)/tags/%c3%ce%ca%c7%d1%e5%c7.htm",
+				km.makeKey("http://www.aluroba.com/tags/%C3%CE%CA%C7%D1%E5%C7.htm"));
 	}
 
 }
