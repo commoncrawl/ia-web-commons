bf9a9e0a1a8d6ed798eaf5c1ce04e1b853aae729
Sebastian Nagel <sebastian@commoncrawl.org>
2024-12-11 18:23:34 +0100

    Remove unused import

--- src/test/java/org/archive/resource/html/ExtractingParseObserverTest.java
+++ src/test/java/org/archive/resource/html/ExtractingParseObserverTest.java
@@ -3,7 +3,6 @@ package org.archive.resource.html;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import org.archive.extract.ExtractingResourceFactoryMapper;
